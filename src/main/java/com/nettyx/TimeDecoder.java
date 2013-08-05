d550e0723945e5b440daf6c2398028b26e424fe0
Saikat Maitra <saikat.maitra@gmail.com>
2013-08-05 14:30:50 +0530

    streaming

---
+++ src/main/java/com/nettyx/TimeDecoder.java
@@ -0,0 +1,25 @@
+package com.nettyx;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 05/08/13
+ * Time: 2:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+
+        out.add(in.readBytes(4)); // (4)
+    }
+}
