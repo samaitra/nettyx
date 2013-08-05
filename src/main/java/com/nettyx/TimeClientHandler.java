a00b37dd42c61e1cb9434daa4ee4fd4b96f69dbe
Saikat Maitra <saikat.maitra@gmail.com>
2013-08-05 13:05:13 +0530

    time client and handler

---
+++ src/main/java/com/nettyx/TimeClientHandler.java
@@ -0,0 +1,35 @@
+package com.nettyx;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 05/08/13
+ * Time: 12:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf m = (ByteBuf) msg; // (1)
+        try {
+            long currentTimeMillis = (m.readInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
